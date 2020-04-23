8f362dcec90f027f364975c113089790fecec2c1
cartes <cristiancartesa@gmail.com>
2020-04-23 17:06:26 -0400

    Admin categories... to be finished tomorrow please

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = Category::all();
+
+        return view('admin.categories');
+
+    }
 }
