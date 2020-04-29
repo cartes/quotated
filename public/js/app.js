e1ee3a8d7d8dc5b7969ee690dd0f0a1abff961e7
cartes <cristiancartesa@gmail.com>
2020-04-29 18:32:17 -0400

    Last version May 29th

--- public/js/app.js
+++ public/js/app.js
@@ -103483,8 +103483,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/cristiancartes/Documents/prototipos/quotated/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/cristiancartes/Documents/prototipos/quotated/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/cristian/Documents/prototipos/quotated/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/cristian/Documents/prototipos/quotated/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ }),
